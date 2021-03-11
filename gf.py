83db810088a51bc9ccfa5e652cd6bf7487bcfaaa
Ekramul Hassan <78520987+mao2116@users.noreply.github.com>
2021-03-11 20:27:23 +0600

    Create gf.py

---
+++ gf.py
@@ -0,0 +1,39 @@
+#!/din/python
+#coder:https://github.com/mao2116/
+#Ekramul Hassan
+import os,sys,time,datetime,random
+from pyfiglet import figlet_format
+from termcolor import colored
+
+
+mao = 'THBD'
+mao = figlet_format(mao, 'slant')
+Mao1 = colored(mao, 'cyan')
+x1 = "2116"
+gf = figlet_format("GF Need?", 'slant')
+gf = colored(gf, 'red')
+em = ("😑😶😑")
+try1 = ("Try Again.")
+wa = colored("wait,,,,,,,,", 'cyan')
+try2 = colored(try1, 'cyan')
+def Mao (z):
+  for word in z + '\n':
+     sys.stdout.write(word)
+     sys.stdout.flush()
+     time.sleep(0.01)
+    
+    
+while True:
+  x2 = input('Pass :')
+  if(x2 == x1):
+    Mao(Mao1)
+    Mao(gf)
+    print(em)
+    print(wa)
+    
+    os.system('xdg-open https://www.facebook.com/groups/365865727812895/?ref=share')
+    break
+  else:
+    #os.system('xdg-open https://m.facebook.com/profile.php?id=100064459306467')
+      Mao(try2)
+      
